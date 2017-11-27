46284879cda01a33a8a1ef7a9f9057fdd6036a45
losamigoss <32457293+losamigoss@users.noreply.github.com>
2017-11-27 09:45:13 +0100

    Update ChanceFelt.java
    Grundet tidspres nedprioriteres antallet af chancekort, og der oprettes nu kun 2 i ChanceFelt klassens konstruktør.
    Derudover er tilføjet nødvendige metoder

--- ChanceFelt.java
+++ ChanceFelt.java
@@ -1,19 +1,29 @@
 package Bræt;
-
+import java.util.Random;
 public class ChanceFelt extends Felt{
+	ChanceKort[] chanceKort = new ChanceKort[2];
+	ChanceKort nuværendeKort = new ChanceKort("Nuværende Kort",10);
 	
 	public ChanceFelt(String navn, int feltNummer) {
 		super(navn,feltNummer);
+		chanceKort[0] = new ChanceKort("Du har spist for meget slik BETAL M2 til banken",0);
+		chanceKort[1] = new ChanceKort("Du har lavet alle dine lektier modtag M2 fra banken.",1);
 	}
 	
-	ChanceKort chance1 = new ChanceKort("Ryk frem til start - modtag 2M",1);
-	ChanceKort chance2 = new ChanceKort("Ryk OP TIL 5 felter frem",2);
-	ChanceKort chance3 = new ChanceKort("Ryk 1 felt frem ELLER tage et chancekort mere",3);
-	ChanceKort chance4 = new ChanceKort("Du har spist for meget slik BETAL M2 til banken",4);
-	ChanceKort chance5 = new ChanceKort("Du løslades fra fængsel uden omkostninger Behold dette kort, indtil du får brug for det.",5);
-	ChanceKort chance6 = new ChanceKort("Ryk frem til Strandpromenaden",6);
-	ChanceKort chance7 = new ChanceKort("Det er din fødselsdag alle giver dig M1 tillykke med fødselsdagen",7);
-	ChanceKort chance8 = new ChanceKort("Du har lavet alle dine lektier modtag M2 fra banken.",8);
-	ChanceKort chance = new ChanceKort("",9);
+	
+	public ChanceKort trækChance() {
+		Random rand = new Random();
+		int randomNum = rand.nextInt((1 - 0) + 1) + 0;
+		return chanceKort[randomNum];
+	}
+	
+	public String getChanceNavn() {
+		return nuværendeKort.getNavn();
+	}
+	
+	public int getNummer() {
+		return nuværendeKort.getNummer();
+	}
 	
 }
+
